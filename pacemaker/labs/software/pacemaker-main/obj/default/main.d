7429cb33b3c480e2aa78567420ba2324676ae63f
LachlanChan <lcha691@aucklanduni.ac.nz>
2024-09-26 15:30:29 +1200

    Folder Actually works

---
+++ pacemaker/labs/software/pacemaker-main/obj/default/main.d
@@ -0,0 +1,37 @@
+obj/default/main.o: main.c ../pacemaker-main_bsp/system.h \
+ ../pacemaker-main_bsp/linker.h \
+ ../pacemaker-main_bsp//HAL/inc/sys/alt_alarm.h \
+ ../pacemaker-main_bsp//HAL/inc/sys/alt_llist.h \
+ ../pacemaker-main_bsp//HAL/inc/alt_types.h \
+ ../pacemaker-main_bsp//HAL/inc/priv/alt_alarm.h \
+ ../pacemaker-main_bsp//HAL/inc/sys/alt_irq.h \
+ ../pacemaker-main_bsp//HAL/inc/nios2.h \
+ ../pacemaker-main_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../pacemaker-main_bsp//HAL/inc/io.h \
+ ../pacemaker-main_bsp//HAL/inc/alt_types.h sccharts.h timing.h
+
+../pacemaker-main_bsp/system.h:
+
+../pacemaker-main_bsp/linker.h:
+
+../pacemaker-main_bsp//HAL/inc/sys/alt_alarm.h:
+
+../pacemaker-main_bsp//HAL/inc/sys/alt_llist.h:
+
+../pacemaker-main_bsp//HAL/inc/alt_types.h:
+
+../pacemaker-main_bsp//HAL/inc/priv/alt_alarm.h:
+
+../pacemaker-main_bsp//HAL/inc/sys/alt_irq.h:
+
+../pacemaker-main_bsp//HAL/inc/nios2.h:
+
+../pacemaker-main_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../pacemaker-main_bsp//HAL/inc/io.h:
+
+../pacemaker-main_bsp//HAL/inc/alt_types.h:
+
+sccharts.h:
+
+timing.h:
